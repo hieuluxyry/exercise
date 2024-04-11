bb79067465c2ba572afae4cb941c0f4a5c036c1e
hieuluxury <traihopham665@gmail.com>
2024-04-12 01:18:18 +0700

    first commit

---
+++ doyouloveme/Myjs.js
@@ -0,0 +1,11 @@
+
+function check() {
+    alert("YES! I LOVE YOU VERY MUCH!!!!")
+ }
+ function noHover() {
+     let x = Math.round( Math.random()*window.innerWidth);
+     let y= Math.round(Math.random()*window.innerHeight);
+     document.getElementById('no').style.left = x+'px';
+     document.getElementById('no').style.top = y+'px';
+ }
+ 
